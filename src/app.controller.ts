ee8ff7b763ad05725caeecf214a8f72c46f19d2f
Bartłomiej Strama <kontakt@bstrama.com>
2023-01-29 17:17:57 +0000

    Some package.json scripts changes && added nodemon.json && .env file support

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
-@Controller()
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() req: Request): string {
+    //return this.appService.getHello();
+    // I know, changes inside this controller are useless :)
+    return req.url;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
-async function bootstrap() {
+dotenv.config();
+const { PORT = 3000 } = process.env;
+
+(async function () {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  await app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}...`);
+  });
+})();
