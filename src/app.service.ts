69fd20c277de7487d07d416d2d263733182a8ab1
sewe2000 <archdevx7d6@sewe-hub.xyz>
2023-02-05 11:38:23 +0100

    Return more information in / GET request

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,12 +5,37 @@ import { DbService } from 'src/db/db.service';
 export class AppService {
   constructor(private readonly prisma: DbService) {}
   async getPosts(): Promise<object> {
+    const columns = {
+      createdAt: true,
+      title: true,
+      text: true,
+      author: {
+        select: {
+          username: true,
+          surname: true,
+          name: true,
+          class_name: true,
+          avatar: true,
+        },
+      },
+    };
+
     const spottedPosts = await this.prisma.spottedPost.findMany({
       orderBy: { createdAt: 'desc' },
+      select: columns,
     });
+
+    (columns as any).UserProject = {
+      select: {
+        Users: true,
+      },
+    };
+
     const projects = await this.prisma.project.findMany({
       orderBy: { createdAt: 'desc' },
+      select: columns,
     });
+
     return {
       spottedPosts,
       projects,
