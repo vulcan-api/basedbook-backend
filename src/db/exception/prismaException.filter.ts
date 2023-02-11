516725abafa8a04d6ace97a9b71b3234160ce57c
sewe2000 <archdevx7d6@sewe-hub.xyz>
2023-02-11 14:39:20 +0100

    Provide some more user friendly message for Prisma NotFoundError

---
+++ src/db/exception/prismaException.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+@Catch(PrismaClientKnownRequestError)
+export class PrismaExceptionFilter implements ExceptionFilter {
+  catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    let message = 'Internal server error';
+    switch (exception.code) {
+      case 'P2025':
+        response.statusCode = HttpStatus.NOT_FOUND;
+        message = exception.message;
+        break;
+      default:
+        response.statusCode = 500;
+    }
+    response.json({
+      status: response.statusCode,
+      message,
+    });
+  }
+}
