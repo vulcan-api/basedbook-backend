ce26b40ab169826702600232c931872e18b2352f
Bartłomiej Strama <kontakt@bstrama.com>
2023-02-24 16:00:51 +0100

    get user spotted posts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Param, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { SpottedService } from '../spotted/spotted.service';
 
 @Controller('/user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private spottedService: SpottedService,
+  ) {}
 
   @Get('/:userId')
   async getPublicInformation(@Param('userId') userId: string) {
     return this.userService.getPublicInformation(parseInt(userId));
+  }
+
+  @Get('/:userId/spottedPosts')
+  async getSpottedPosts(@Param('userId') userId: string) {
+    return this.spottedService.getPostList(0, 999, parseInt(userId));
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { SkillsModule } from './skills/skills.module';
 import { SettingsModule } from './settings/settings.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { SpottedService } from '../spotted/spotted.service';
 
 @Module({
   imports: [SkillsModule, SettingsModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, SpottedService],
 })
 export class UserModule {}
