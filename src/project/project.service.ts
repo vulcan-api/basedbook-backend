d7cc7458d0aa7e3a644eb5797b708711d32e586a
Bartłomiej Strama <kontakt@bstrama.com>
2023-03-10 16:00:09 +0100

    get all projects returns authorId

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -29,6 +29,7 @@ export class ProjectService {
             name: true,
             surname: true,
             username: true,
+            id: true,
           },
         },
         UserProject: {
