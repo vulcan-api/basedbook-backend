b5d181926cbae1be7b8c71e7ef01923395de44ce
Bartłomiej Strama <kontakt@bstrama.com>
2023-03-10 09:23:27 +0100

    Returning comment's when asking for all spotted posts

--- src/spotted/spotted.service.ts
+++ src/spotted/spotted.service.ts
@@ -14,26 +14,69 @@ export class SpottedService {
   ): Promise<any[]> {
     const { prisma } = this;
 
-    const spottedPosts: any[] = await prisma.$queryRaw`
-        SELECT DISTINCT s.id,
-       "createdAt",
-       title,
-       text,
-       u.username,
-       "isAnonymous",
-       (SELECT count(l) FROM "SpottedLikes" l WHERE l."postId" = s.id) AS "likes",
-       (SELECT count(l) FROM "SpottedLikes" l WHERE l."postId" = s.id AND l."userId" = ${userId}) AS "isLiked"
-            FROM "SpottedPost" s 
-            LEFT JOIN "SpottedLikes" l ON s.id = l."postId" 
-            LEFT JOIN "User" u ON s."authorId" = u.id 
-            ORDER BY s."createdAt" desc
-            OFFSET ${skip} LIMIT ${take}`;
+    const spottedPosts = await prisma.spottedPost.findMany({
+      skip,
+      take,
+      orderBy: {
+        createdAt: 'desc',
+      },
+      select: {
+        id: true,
+        createdAt: true,
+        title: true,
+        text: true,
+        isAnonymous: true,
+        author: {
+          select: {
+            username: true,
+            id: true,
+          },
+        },
+        Comment: true,
+        SpottedLikes: true,
+      },
+    });
 
     return spottedPosts.map((post: any) => {
-      post.likes = parseInt(post.likes);
-      post.isLiked = Boolean(parseInt(post.isLiked));
+      post.isOwned = post.author.id === userId;
+      if (post.isAnonymous) delete post.author;
+
+      post.likes = post.SpottedLikes.length;
+      post.isLiked = post.SpottedLikes.some(
+        (like: any) => like.userId === userId,
+      );
+      post.comments = this.nestReplies(post.Comment);
+      delete post.Comment;
+      console.log(post.comments);
       return post;
     });
+  }
+
+  private nestReplies(comments: any[], parentId?: number): object | null {
+    let processingComments: any[];
+    if (!parentId) {
+      processingComments = comments.filter((comment) => !comment.parentId);
+    } else {
+      processingComments = comments.filter(
+        (comment) => comment.parentId === parentId,
+      );
+    }
+    const answer: any = {};
+
+    processingComments.forEach((processingElement) =>
+      comments.splice(
+        comments.findIndex((e: any) => e === processingElement),
+        1,
+      ),
+    );
+
+    processingComments.forEach((processingElement) => {
+      answer[processingElement.id] = Object.assign(processingElement, {
+        replies: this.nestReplies(comments, processingElement.id),
+      });
+    });
+    if (!Object.keys(answer).length) return null;
+    return answer;
   }
 
   async getUsersPosts(skip: number, take: number, userId: number) {
@@ -142,6 +185,12 @@ export class SpottedService {
         text,
         parentId: commentId,
       },
+    });
+  }
+
+  async deleteComment(commentId: number, userId: number): Promise<void> {
+    await this.prisma.comment.deleteMany({
+      where: { id: commentId, authorId: userId },
     });
   }
 
