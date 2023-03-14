4a33ba6ce5b85427661f90d96b5d2714fb712124
sewe2000 <archdevx7d6@sewe-hub.xyz>
2023-03-14 21:09:29 +0100

    Add facebook.strategy.ts

---
+++ src/auth/strategy/facebook.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-facebook';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      clientID: configService.getOrThrow('FB_APP_ID'),
+      clientSecret: configService.getOrThrow('FB_APP_SECRET'),
+      callbackURL: configService.getOrThrow('FB_CALLBACK_URL'),
+      profileFields: ['emails', 'name'],
+      scope: ['email'],
+    });
+  }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ) {
+    const { name, emails, id } = profile;
+    const user = {
+      emails,
+      name,
+      id,
+      accessToken,
+    };
+    done(null, user);
+  }
+}
