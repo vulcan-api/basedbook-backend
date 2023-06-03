4294b9976b3537d33ce6f6acad1368e26e44a65e
Tomasz Mamala <tomaszmamala@gmail.com>
2023-06-03 16:46:04 +0200

    temportary commented chat function

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -19,40 +19,40 @@ import { EditMessageDto } from './dto/editMessage.dto';
 @Controller('chat')
 export class ChatController {
   constructor(private chatService: ChatService) {}
-  @Get('conversation/:receiverId')
-  async getConversation(
-    @Param('receiverId') receiverId: number,
-    @GetUser() user: JwtAuthDto,
-  ) {
-    return await this.chatService.getConversation(user.userId, receiverId);
-  }
-  @Post('send')
-  async sendMessage(@Body() dto: SendMessageDto, @GetUser() user: JwtAuthDto) {
-    await this.chatService.sendMessage(
-      dto.content,
-      user.userId,
-      dto.receiverId,
-    );
-    //TODO
-    //WebSockets
-  }
-  @Put('edit/:messageId')
-  async editMessage(
-    @GetUser() user: JwtAuthDto,
-    @Body() dto: EditMessageDto,
-    @Param('messageId') messageId: string,
-  ) {
-    return await this.chatService.editMessage(
-      user.userId,
-      +messageId,
-      dto.content,
-    );
-  }
-  @Delete('delete/:messageId')
-  async deleteMessage(
-    @GetUser() user: JwtAuthDto,
-    @Param('messageId') messageId: string,
-  ) {
-    return await this.chatService.deleteMessage(user.userId, +messageId);
-  }
+  // @Get('conversation/:receiverId')
+  // async getConversation(
+  //   @Param('receiverId') receiverId: number,
+  //   @GetUser() user: JwtAuthDto,
+  // ) {
+  //   return await this.chatService.getConversation(user.userId, receiverId);
+  // }
+  // @Post('send')
+  // async sendMessage(@Body() dto: SendMessageDto, @GetUser() user: JwtAuthDto) {
+  //   await this.chatService.sendMessage(
+  //     dto.content,
+  //     user.userId,
+  //     dto.receiverId,
+  //   );
+  //   //TODO
+  //   //WebSockets
+  // }
+  // @Put('edit/:messageId')
+  // async editMessage(
+  //   @GetUser() user: JwtAuthDto,
+  //   @Body() dto: EditMessageDto,
+  //   @Param('messageId') messageId: string,
+  // ) {
+  //   return await this.chatService.editMessage(
+  //     user.userId,
+  //     +messageId,
+  //     dto.content,
+  //   );
+  // }
+  // @Delete('delete/:messageId')
+  // async deleteMessage(
+  //   @GetUser() user: JwtAuthDto,
+  //   @Param('messageId') messageId: string,
+  // ) {
+  //   return await this.chatService.deleteMessage(user.userId, +messageId);
+  // }
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -4,85 +4,85 @@ import { DbService } from '../db/db.service';
 @Injectable()
 export class ChatService {
   constructor(private readonly prisma: DbService) {}
-  public async sendMessage(
-    content: string,
-    senderId: number,
-    receiverId: number,
-  ): Promise<string> {
-    try {
-      await this.prisma.message.create({
-        data: {
-          content: content,
-          senderId: senderId,
-          receiverId: receiverId,
-        },
-      });
-      return 'Message sent';
-    } catch (e) {
-      console.log(e);
-      return 'Error';
-    }
-  }
-  public async getConversation(
-    senderId: number,
-    receiverId: number,
-  ): Promise<any> {
-    try {
-      return await this.prisma.message.findMany({
-        where: {
-          OR: [
-            {
-              senderId: senderId,
-              receiverId: receiverId,
-            },
-            {
-              senderId: receiverId,
-              receiverId: senderId,
-            },
-          ],
-        },
-        orderBy: {
-          sendTime: 'asc',
-        },
-        select: {
-          content: true,
-          senderId: true,
-          receiverId: true,
-          sendTime: true,
-        },
-      });
-    } catch (e) {
-      console.log(e);
-      return 'Error';
-    }
-  }
-  public async deleteMessage(
-    senderId: number,
-    messageId: number,
-  ): Promise<any> {
-    try {
-      await this.prisma.message.deleteMany({
-        where: { id: messageId, senderId: senderId },
-      });
-    } catch (e) {
-      console.log(e);
-      return 'Error';
-    }
-  }
+  // public async sendMessage(
+  //   content: string,
+  //   senderId: number,
+  //   receiverId: number,
+  // ): Promise<string> {
+  //   try {
+  //     await this.prisma.message.create({
+  //       data: {
+  //         content: content,
+  //         senderId: senderId,
+  //         receiverId: receiverId,
+  //       },
+  //     });
+  //     return 'Message sent';
+  //   } catch (e) {
+  //     console.log(e);
+  //     return 'Error';
+  //   }
+  // }
+  // public async getConversation(
+  //   senderId: number,
+  //   receiverId: number,
+  // ): Promise<any> {
+  //   try {
+  //     return await this.prisma.message.findMany({
+  //       where: {
+  //         OR: [
+  //           {
+  //             senderId: senderId,
+  //             receiverId: receiverId,
+  //           },
+  //           {
+  //             senderId: receiverId,
+  //             receiverId: senderId,
+  //           },
+  //         ],
+  //       },
+  //       orderBy: {
+  //         sendTime: 'asc',
+  //       },
+  //       select: {
+  //         content: true,
+  //         senderId: true,
+  //         receiverId: true,
+  //         sendTime: true,
+  //       },
+  //     });
+  //   } catch (e) {
+  //     console.log(e);
+  //     return 'Error';
+  //   }
+  // }
+  // public async deleteMessage(
+  //   senderId: number,
+  //   messageId: number,
+  // ): Promise<any> {
+  //   try {
+  //     await this.prisma.message.deleteMany({
+  //       where: { id: messageId, senderId: senderId },
+  //     });
+  //   } catch (e) {
+  //     console.log(e);
+  //     return 'Error';
+  //   }
+  // }
 
-  public async editMessage(
-    senderId: number,
-    messageId: number,
-    content: string,
-  ): Promise<any> {
-    try {
-      await this.prisma.message.updateMany({
-        where: { id: messageId, senderId: senderId },
-        data: { content: content, isEdited: true },
-      });
-    } catch (e) {
-      console.log(e);
-      return 'Error';
-    }
-  }
+  // public async editMessage(
+  //   senderId: number,
+  //   messageId: number,
+  //   content: string,
+  // ): Promise<any> {
+  //   try {
+  //     await this.prisma.message.updateMany({
+  //       where: { id: messageId, senderId: senderId },
+  //       data: { content: content, isEdited: true },
+  //     });
+  //   } catch (e) {
+  //     console.log(e);
+  //     return 'Error';
+  //   }
+  // }
 }
