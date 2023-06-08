5fc3693ea024a948b70bd81d6a538d467cff8e27
sewe2000 <archdevx7d6@sewe-hub.xyz>
2023-06-08 19:25:37 +0200

    Fix ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'basedbook-backend',
-      script: './dist/main.js',
+      script: './dist/src/main.js',
       instances: 'max',
       exec_mode: 'cluster',
     },
